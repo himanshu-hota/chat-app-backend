c47b31ab96932ea77f06c03b736d0b9a912b19b3
Himanshu Hota <Himanshuhota55@gmail.com>
2024-02-08 19:04:51 +0530

    cors fix 0.9

--- server.js
+++ server.js
@@ -21,58 +21,41 @@ import { app, server } from './socket/socket.js';
 const port = process.env.PORT || 5000;
 
 dotenv.config(); // to get accesst o env variables
-
-// const __dirname = path.resolve();
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-
-// const corsOptions = {
-//     origin: '*',
-// }
-
-// app.use(cors(corsOptions));
 app.use(express.json()); // to parse json payload from req.body.
-app.use(cookieParser()); // to parse the incoming cookie.
 
-// app.get('/', (req, res) => {
-//     res.send('Server is working');
-// });
+
+const corsOptions = {
+    origin: '*',
+}
+
+app.use(cors(corsOptions));
+
+
+app.get('/', (req, res) => {
+    res.send('Server is working');
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-// serve static files
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
-// app.use(express.static(path.join(__dirname, '/frontend/public')));
 
 // default route
 app.use('*', (req, res, next) => {
-    // res.sendFile(path.join((__dirname, "frontend", "dist", "index.html")));
     return res.status(404).json({ message: 'You reached the space station!!!' });
 })
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
-    // console.error(err.stack);
+
     return res.status(500).send('Something went wrong!');
-    // res.sendFile(path.join((__dirname, "frontend", "public", "404.html")));
-    // return res.status(500).json({ message: 'Something went wrong!', error: err });
+
 });
 
 server.listen(port, () => {
     try {
         connectToMongoDB();
-        // console.log('Server started at localhost:', port);
     } catch (err) {
-        // console.log("Server establishment failed");
-        // console.log(err);
         res.status(500).send('Something went wrong!');
     }
 })
